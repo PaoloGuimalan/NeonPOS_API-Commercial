4de271f54ca525699c3cf246de13d7d0bcef3283
Paolo <pauloportes.guimalan187@gmail.com>
2024-06-06 11:43:46 +0800

    dependencies has been set up

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import mysql from "mysql";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import path from "path";
+import socketIO from "socket.io";
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    limit: "200mb",
+    extended: false
+}));
+app.use(bodyParser.json({
+    limit: "200mb"
+}));
+app.use(express.json());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server Running: ${PORT}`)
+    // connectMongo().then(() => {
+    //     console.log(`Connected to MongoDB`)
+    // }).catch((err) => {
+    //     console.log(err)
+    // })
+})
